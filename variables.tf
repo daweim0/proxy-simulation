4158947ee84678a6fe7afa52ca1d320bac57f2c3
Shashank Barsin <shasb@microsoft.com>
2021-03-15 22:21:51 +0000

    Updated variables.tf to use new extension .whl files

--- variables.tf
+++ variables.tf
@@ -10,13 +10,13 @@ variable "publickeypath" {
 
 variable "connectedk8s_source" {
   type    = string
-  default = "https://shasbextensions.blob.core.windows.net/extensions/connectedk8s-0.3.5-py2.py3-none-any.whl"
+  default = "https://arcextensions.blob.core.windows.net/extensions/connectedk8s-0.3.8-py2.py3-none-any.whl"
   description = "Location of .whl file from where the extension is to be installed. If left unspecified, latest version is used"
 }
 
 variable "k8s_extension_source" {
   type    = string
-  default = "https://shasbextensions.blob.core.windows.net/extensions/k8s_extension-0.1PP.8-py2.py3-none-any.whl"
+  default = "https://arcextensions.blob.core.windows.net/extensions/k8s_extension-0.1PP.13-py2.py3-none-any.whl"
   description = "Location of .whl file from where the extension is to be installed. If left unspecified, latest version is used"
 }
 
